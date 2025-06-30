8721c29f79d58d04c695b4ffa965bc440102bbf7
eleonoraferns <140112509+eleonoraferns@users.noreply.github.com>
2025-07-01 00:56:06 +0530

    Update chains

--- chains/creative.py
+++ chains/creative.py
@@ -1,9 +1,9 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 
-def creative_chain():
+def creative_chain(temp):
 
-    llm=Ollama(model="llama3.2", temperature=0.9)
+    llm=Ollama(model="llama3.2", temperature=temp)
 
     prompt = ChatPromptTemplate.from_messages([
         ("system", "You are a creative writing mentor. Your role is to help writers break through blocks by asking imaginative, open-ended questions and gently offering idea prompts that spark inspiration and pique their creativity. Do not provide full content or write for them unless they explicitly ask. Your responses should encourage exploration, creativity, and confidence. If the user seems deeply stuck, suggest an unexpected perspective or alternative angle to help them reframe their thinking. Always answer with warmth, encouragement, and honesty"),
